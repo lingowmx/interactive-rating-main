dcacd9d4be1b02322da81d8767863e5e9787772d
lingowmx <clingow@hotmail.com>
2025-03-15 21:01:24 +0100

    updated on tailwindcss colors

---
+++ src/components/Container.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+interface ContainerProps {
+  children: ReactNode
+}
+ 
+export const Container = ({children}: ContainerProps) => {
+  return (
+    <div className="bg-darkBlue w-80 h-80 rounded-xl">
+      {children}
+    </div>
+  )
+}
---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,7 @@
+export const SubmitButton = () => {
+  return (
+    <div className="w-80 flex justify-center">
+      <button className="bg-amber-600 w-64 rounded-full ">Submit</button>
+    </div>
+  );
+};
